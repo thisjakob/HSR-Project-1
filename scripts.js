d7604b8c74e93cc421f48cb502add56b748c9f55
gip <pascal@GipAir.local>
2015-05-22 20:31:19 +0200

    set item to change in localStorage

---
+++ scripts.js
@@ -0,0 +1,6 @@
+if (localStorage.getItem("cmd") === "save") {
+    var title = localStorage.getItem("title");
+    var desc = localStorage.getItem("desc");
+    var importance = localStorage.getItem("importance");
+    var date = localStorage.getItem("date");
+}
