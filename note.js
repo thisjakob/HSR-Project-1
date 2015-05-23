0cbfe3b037ba296c0329433779150302baa51bbb
gip <pascal@GipAir.local>
2015-05-23 07:09:01 +0200

    Notes in Array
    localStorage item „cmd“ define, if notes is new or edit note. cmd
    define position of array

--- note.js
+++ note.js
@@ -4,19 +4,41 @@ function cancel() {
     window.location.replace("index.html");
 };
 
+/*var notes = '{ "notes" : ['
+    + '{ "title":' + document.getElementById("title").value)
++ '{ "desc":' + document.getElementById("desc").value)
++ '{ "importance":' + document.getElementById("importance").value)
++ '{ "date":' + document.getElementById("date").value)
++ '} ]}';*/
 function save() {
-    // read items from inputs and save it to localStorage
+    if (cmd !== "new") {
+        notes.splice(parseInt(cmd), 1);
+    }
+    var note = '{ "title":"' + document.getElementById("title").value
+        + '", "desc":"' + document.getElementById("desc").value
+        + '", "importance":"' + document.getElementById("importance").value
+        + '", "date":"' + document.getElementById("date").value
+        + '"}';
+    notes.push(note);
+    localStorage.setItem("notes", JSON.stringify(notes));
+
+/*    // read items from inputs and save it to localStorage
     localStorage.setItem("title",document.getElementById("title").value);
     localStorage.setItem("desc",document.getElementById("desc").value)
     localStorage.setItem("importance",document.getElementById("importance").value)
     localStorage.setItem("date",document.getElementById("date").value)
     // activate index.html
-    localStorage.setItem("cmd", "save");
+    localStorage.setItem("cmd", "save");*/
     window.location.replace("index.html");
 }
 
-// read items from localStorage and put it in inputs
-document.getElementById("title").value = localStorage.getItem("title");
-document.getElementById("desc").value = localStorage.getItem("desc");
-document.getElementById("importance").value = localStorage.getItem("importance");
-document.getElementById("date").value = localStorage.getItem("date");
+var notes = JSON.parse(localStorage.getItem("notes"));
+var cmd = localStorage.getItem("cmd");
+if (cmd !== "new") {
+    // read items from localStorage and set inputs
+    var note = JSON.parse(notes[parseInt(cmd)]);
+    document.getElementById("title").value = note.title;
+    document.getElementById("desc").value = note.desc;
+    document.getElementById("importance").value = note.importance;
+    document.getElementById("date").value = note.date;
+}
--- scripts.js
+++ scripts.js
@@ -1,6 +1,19 @@
-if (localStorage.getItem("cmd") === "save") {
-    var title = localStorage.getItem("title");
-    var desc = localStorage.getItem("desc");
-    var importance = localStorage.getItem("importance");
-    var date = localStorage.getItem("date");
+//localStorage.removeItem("notes");
+var notes = localStorage.getItem("notes");
+if( !notes )
+{
+    localStorage.setItem("notes", JSON.stringify([]));
+    var notes = localStorage.getItem("notes");
+}
+notes = JSON.parse(notes);
+if (notes.length > 0) {
+    localStorage.setItem("cmd", "0");
+    var note = JSON.parse(notes[notes.length - 1]);
+    var title = note.title;
+    var desc = note.desc;
+    var importance = note.importance;
+    var date = note.date;
+    console.log("Note 0: " + title + "; " + desc + "; " + importance + "; " + date);
+} else {
+    localStorage.setItem("cmd", "new");
 }
